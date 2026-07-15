e9b328ae59078791a6724e5fa8ea62ee6538f20e
ARCHANA-20279 <archana552m@gmail.com>
2026-07-15 21:47:24 +0530

    completed

--- app.js
+++ app.js
@@ -12,6 +12,37 @@ app.use(Cors())
 
 Mongoose.connect("mongodb://archana:archana@ac-58g5bmi-shard-00-00.5ys18b0.mongodb.net:27017,ac-58g5bmi-shard-00-01.5ys18b0.mongodb.net:27017,ac-58g5bmi-shard-00-02.5ys18b0.mongodb.net:27017/blogAppDb?ssl=true&replicaSet=atlas-cngkhq-shard-0&authSource=admin&appName=Cluster0://")
 
+app.post("/signin",async(request,response)=>{
+
+    let input=request.body
+    let result=userModel.find({email:request.body.email}).then((items)=>{
+
+if (items.length>0){
+const PasswordValiator=Bcrypt.compareSync(request.body.password,items[0].password)
+
+if (PasswordValiator){
+
+
+
+    Jsonwebtoken.sign({email:request.body.email},"blogApp",{expiresIn:"1d"},(error,token)=>{
+        if (error){
+            response.json({"status":"error generating token"})
+        } else {
+            response.json({"status":"success","token":token,"userId":items[0]._id})
+        }
+    })
+}else{
+    response.json({"status":"invalid password"})
+    
+    
+}
+}
+else{
+    response.json({"status":"user not found"})
+}
+    }
+).catch((error)=>{})
+})
 
 app.post("/signup",async(request,response)=>{
     let input=request.body
